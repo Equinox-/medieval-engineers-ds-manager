7f64533c427c81214038eeae003a28693d783bd7
Westin Miller <equinoxscripts@gmail.com>
2021-02-13 17:17:57 -0800

    CI Utils

---
+++ Meds.Watchdog/CiUtils.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Meds.Watchdog.Steam;
+using Meds.Watchdog.Tasks;
+using SteamKit2;
+
+namespace Meds.Watchdog
+{
+    public static class CiUtils
+    {
+        public static async Task RestoreGameBinaries(string target)
+        {
+            var downloader = new SteamDownloader(SteamConfiguration.Create(x => { }));
+            await downloader.LoginAsync();
+            try
+            {
+                await downloader.InstallAppAsync(UpdateTask.MedievalDsAppId, UpdateTask.MedievalDsDepotId, "public", target, 4,
+                    path => path.StartsWith("DedicatedServer64"));
+            }
+            finally
+            {
+                await downloader.LogoutAsync();
+            }
+        }
+    }
+}
