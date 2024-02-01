6b0b7b2980100c4e69cf6e38534499c5d08e1a0f
Westin Miller <equinoxscripts@gmail.com>
2024-02-01 15:00:26 -0800

    Set cache control

--- Meds.Watchdog/Utils/RtcFileSharing.cs
+++ Meds.Watchdog/Utils/RtcFileSharing.cs
@@ -366,7 +366,7 @@ namespace Meds.Watchdog.Utils
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     SendMessage(new WsAddIceCandidate
                     {
-                        Candidate = new RTCIceCandidateInit()
+                        Candidate = new RTCIceCandidateInit
                         {
                             sdpMid = candidate.sdpMid ?? candidate.sdpMLineIndex.ToString(),
                             sdpMLineIndex = candidate.sdpMLineIndex,
