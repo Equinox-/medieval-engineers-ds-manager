a8f3aefe4fee259a81eb7b9fb0a69530b921c365
Westin Miller <equinoxscripts@gmail.com>
2024-12-09 09:00:03 -0800

    0.7.4

--- Meds.Wrapper/Shim/MecPatches.cs
+++ Meds.Wrapper/Shim/MecPatches.cs
@@ -533,7 +533,7 @@ namespace Meds.Wrapper.Shim
 
     // https://communityedition.medievalengineers.com/mantis/view.php?id=480
     [HarmonyPatch(typeof(MyEntityStatComponent.DelayedEffect), "HandleTick")]
-    [AlwaysPatch]
+    [AlwaysPatch(VersionRange = "[,0.7.4)")]
     public static class SkipUnloadedDelayedEffects
     {
         public static bool Prefix(MyEntityStatComponent.DelayedEffect __instance) => __instance.StatComponent?.Entity?.InScene ?? false;
