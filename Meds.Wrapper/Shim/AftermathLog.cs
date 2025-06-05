28f959a6322587f4d3a3a1bdb8da0ad1c08288fb
Westin Miller <equinoxscripts@gmail.com>
2025-06-04 20:41:56 -0700

    IWP aftermath

--- Meds.Wrapper/Shim/AftermathLog.cs
+++ Meds.Wrapper/Shim/AftermathLog.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using HarmonyLib;
 using Havok;
+using Medieval.World.Persistence;
 using Sandbox.Engine.Physics;
 using Sandbox.Game.Components;
 using Sandbox.Game.EntityComponents.Grid;
@@ -20,44 +21,57 @@ namespace Meds.Wrapper.Shim
     public static class AftermathLog
     {
         // Meds.Wrapper.Shim.AftermathLog._events
-        private const int MaxEventCount = 1024;
+        private const int MaxEventCount = 8192;
         private static readonly Queue<AftermathEvent> _events = new Queue<AftermathEvent>();
 
-        [DebuggerDisplay("{Tick}, {Type}, {EntityOneName}, {EntityTwoName}")]
+        [DebuggerDisplay("{Tick}, {Type}, {ObjOneName}, {ObjTwoName}")]
         private struct AftermathEvent
         {
             public int Tick;
 
             public AftermathType Type;
 
-            public EntityId EntityOne;
-            public string EntityOneDef;
-            public string EntityOneName;
+            public ulong ObjOne;
+            public string ObjOneDef;
+            public string ObjOneName;
 
-            public EntityId EntityTwo;
-            public string EntityTwoDef;
-            public string EntityTwoName;
+            public ulong ObjTwo;
+            public string ObjTwoDef;
+            public string ObjTwoName;
         }
 
         private enum AftermathType
         {
-            SetShape,
-            AddConstraint,
-            RemoveConstraint,
-            AddBodyToWorld,
-            RemoveBodyFromWorld,
+            HavokSetShape,
+            HavokConstraintAdd,
+            HavokConstraintRemove,
+            HavokBodyAdd,
+            HavokBodyRemove,
+
+            IwpEntityAdd,
+            IwpEntityRemove,
+            IwpGroupAdd,
+            IwpGroupRemove,
         }
 
         private static AftermathEvent CreateEvent(AftermathType type, MyEntity a, MyEntity b = null) => new AftermathEvent
         {
             Tick = MySession.Static?.GameplayFrameCounter ?? 0,
             Type = type,
-            EntityOne = a?.Id ?? default,
-            EntityOneDef = a?.DefinitionId?.SubtypeName,
-            EntityOneName = a?.DebugName,
-            EntityTwo = b?.Id ?? default,
-            EntityTwoDef = b?.DefinitionId?.SubtypeName,
-            EntityTwoName = b?.DebugName,
+            ObjOne = a?.Id.Value ?? 0,
+            ObjOneDef = a?.DefinitionId?.SubtypeName,
+            ObjOneName = a?.DebugName,
+            ObjTwo = b?.Id.Value ?? 0,
+            ObjTwoDef = b?.DefinitionId?.SubtypeName,
+            ObjTwoName = b?.DebugName,
+        };
+
+        private static AftermathEvent CreateEvent(AftermathType type, MyGroup group) => new AftermathEvent
+        {
+            Tick = MySession.Static?.GameplayFrameCounter ?? 0,
+            Type = type,
+            ObjOne = group?.Id.Value ?? 0,
+            ObjOneDef = group?.GetType().Name,
         };
 
         private static AftermathEvent CreateEvent(AftermathType type, HkRigidBody a, HkRigidBody b = null) => CreateEvent(
@@ -65,42 +79,35 @@ namespace Meds.Wrapper.Shim
             (a?.UserObject as MyEntityComponent)?.Entity,
             (b?.UserObject as MyEntityComponent)?.Entity);
 
-        [MethodImpl(MethodImplOptions.Synchronized)]
         private static void Add(in AftermathEvent evt)
         {
-            while (_events.Count >= MaxEventCount)
-                _events.Dequeue();
-            _events.Enqueue(evt);
+            lock (_events)
+            {
+                while (_events.Count >= MaxEventCount)
+                    _events.Dequeue();
+                _events.Enqueue(evt);
+            }
         }
 
         [HarmonyPatch(typeof(MyGridRigidBodyComponent), nameof(MyClusterTree.IMyActivationHandler.Activate))]
         [AlwaysPatch]
         public static class AddGridBodyToWorld
         {
-            public static void Postfix(MyGridRigidBodyComponent __instance)
-            {
-                Add(CreateEvent(AftermathType.AddBodyToWorld, __instance.Entity));
-            }
+            public static void Postfix(MyGridRigidBodyComponent __instance) => Add(CreateEvent(AftermathType.HavokBodyAdd, __instance.Entity));
         }
 
         [HarmonyPatch(typeof(MyGridRigidBodyComponent), nameof(MyClusterTree.IMyActivationHandler.Deactivate))]
         [AlwaysPatch]
         public static class RemoveGridBodyFromWorld
         {
-            public static void Prefix(MyGridRigidBodyComponent __instance)
-            {
-                Add(CreateEvent(AftermathType.RemoveBodyFromWorld, __instance.Entity));
-            }
+            public static void Prefix(MyGridRigidBodyComponent __instance) => Add(CreateEvent(AftermathType.HavokBodyRemove, __instance.Entity));
         }
 
         [HarmonyPatch(typeof(MyGridRigidBodyComponent), "SetShape")]
         [AlwaysPatch]
         public static class SetGridShape
         {
-            public static void Prefix(MyGridRigidBodyComponent __instance)
-            {
-                Add(CreateEvent(AftermathType.SetShape, __instance.Entity));
-            }
+            public static void Prefix(MyGridRigidBodyComponent __instance) => Add(CreateEvent(AftermathType.HavokSetShape, __instance.Entity));
         }
 
         [HarmonyPatch(typeof(MyPhysicsBody), nameof(MyClusterTree.IMyActivationHandler.Activate))]
@@ -113,7 +120,7 @@ namespace Meds.Wrapper.Shim
                 if ((__instance.Flags & RigidBodyFlag.RBF_DISABLE_COLLISION_RESPONSE) != 0
                     || __instance.Container?.Get<MyUseObjectsComponent>()?.DetectorPhysics == __instance)
                     return;
-                Add(CreateEvent(AftermathType.AddBodyToWorld, __instance.Entity));
+                Add(CreateEvent(AftermathType.HavokBodyAdd, __instance.Entity));
             }
         }
 
@@ -127,7 +134,7 @@ namespace Meds.Wrapper.Shim
                 if ((__instance.Flags & RigidBodyFlag.RBF_DISABLE_COLLISION_RESPONSE) != 0
                     || __instance.Container?.Get<MyUseObjectsComponent>()?.DetectorPhysics == __instance)
                     return;
-                Add(CreateEvent(AftermathType.RemoveBodyFromWorld, __instance.Entity));
+                Add(CreateEvent(AftermathType.HavokBodyRemove, __instance.Entity));
             }
         }
 
@@ -135,20 +142,44 @@ namespace Meds.Wrapper.Shim
         [AlwaysPatch]
         public static class AddConstraintToWorld
         {
-            public static void Prefix(HkConstraint constraint)
-            {
-                Add(CreateEvent(AftermathType.AddConstraint, constraint.RigidBodyA, constraint.RigidBodyB));
-            }
+            public static void Prefix(HkConstraint constraint) =>
+                Add(CreateEvent(AftermathType.HavokConstraintAdd, constraint.RigidBodyA, constraint.RigidBodyB));
         }
 
         [HarmonyPatch(typeof(HkWorld), nameof(HkWorld.RemoveConstraint))]
         [AlwaysPatch]
         public static class RemoveConstraintFromWorld
         {
-            public static void Prefix(HkConstraint constraint)
-            {
-                Add(CreateEvent(AftermathType.RemoveConstraint, constraint.RigidBodyA, constraint.RigidBodyB));
-            }
+            public static void Prefix(HkConstraint constraint) =>
+                Add(CreateEvent(AftermathType.HavokConstraintRemove, constraint.RigidBodyA, constraint.RigidBodyB));
+        }
+
+        [HarmonyPatch("Medieval.World.Persistence.MyEntityGridDatabase+EntityTracker", "Add")]
+        [AlwaysPatch]
+        public static class AddEntityToDatabase
+        {
+            public static void Prefix(MyEntity entity) => Add(CreateEvent(AftermathType.IwpEntityAdd, entity));
+        }
+
+        [HarmonyPatch("Medieval.World.Persistence.MyEntityGridDatabase+EntityTracker", "Remove")]
+        [AlwaysPatch]
+        public static class RemoveEntityFromDatabase
+        {
+            public static void Prefix(MyEntity entity) => Add(CreateEvent(AftermathType.IwpEntityRemove, entity));
+        }
+
+        [HarmonyPatch("Medieval.World.Persistence.MyEntityGridDatabase+GroupTracker", "Add")]
+        [AlwaysPatch]
+        public static class AddGroupToDatabase
+        {
+            public static void Prefix(MyGroup group) => Add(CreateEvent(AftermathType.IwpGroupAdd, group));
+        }
+
+        [HarmonyPatch("Medieval.World.Persistence.MyEntityGridDatabase+GroupTracker", "Remove")]
+        [AlwaysPatch]
+        public static class RemoveGroupFromDatabase
+        {
+            public static void Prefix(MyGroup group) => Add(CreateEvent(AftermathType.IwpGroupRemove, group));
         }
     }
 }
